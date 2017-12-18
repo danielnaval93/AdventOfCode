76ddc7625978c0527759f45c550f17cf8a90c361
Daniel Naval <danielnaval93@gmail.com>
2017-12-17 21:48:22 -0500

    Solved Day 7

--- src/advent/code/Main.java
+++ src/advent/code/Main.java
@@ -1,48 +1,303 @@
-package com.company;
+package advent.code;
 import java.util.*;
+import java.util.regex.*;
 import java.io.*;
+
+
 
 public class Main {
 
+    public class Disc {
+        String name;
+        int weight;
+        ArrayList<Disc> children;
+        public Disc(String _name, int _weight) {
+            name = _name;
+            weight = _weight;
+            children = new ArrayList<Disc>();
+        }
+    }
+
     public static void main(String[] args) {
-        questionFive();
+        Main m = new Main();
+        m.questionSeven();
 
     }
 
-    public static void questionFive() {
-        String fileName = System.getProperty("user.dir") + "\\src\\com\\company\\questionFive.txt";
+    public void questionSeven() {
+        String fileName = System.getProperty("user.dir") + "\\src\\advent\\code\\questionSeven.txt";
+        String pattern = "([a-z]+) \\(([0-9]+)\\)( -> ([a-z]+)(, ([a-z]+))*)?";
+        Pattern p = Pattern.compile(pattern);
+        HashSet<String> discs = new HashSet<String>();
+        HashSet<String> allChildren = new HashSet<String>();
+        HashSet<Disc> allDiscs = new HashSet<Disc>();
+        HashMap<String, Disc> discMap = new HashMap<String, Disc>();
+        String line;
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null) {
+                Matcher m = p.matcher(line);
+                if ( m.matches() ) {
+                    String discName = m.group(1);
+                    int discWeight = Integer.parseInt(m.group(2));
+                    if (m.group(3) != null) {
+                        String childrenString = m.group(3);
+                        // remove the -> part
+                        childrenString = childrenString.substring(4);
+                        String[] children = childrenString.split(", ");
+                        for (int index = 0; index < children.length; index++) {
+                            allChildren.add(children[index]);
+                        }
+                    }
+                    if (discMap.containsKey(discName)) {
+                        discMap.get(discName).weight = discWeight;
+                    } else {
+                        discMap.put(discName, new Disc(discName, discWeight));
+                    }
+                    discs.add(discName);
+                }
+            }
+            bufferedReader.close();
+
+            fileReader = new FileReader(fileName);
+            bufferedReader = new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null) {
+                Matcher m = p.matcher(line);
+                if ( m.matches() ) {
+                    if (m.group(3) != null ) {
+                        String discName = m.group(1);
+                        String childrenString = m.group(3);
+                        // remove the -> part
+                        childrenString = childrenString.substring(4);
+                        String[] children = childrenString.split(", ");
+                        ArrayList<Disc> childDiscs = new ArrayList<Disc>();
+                        for (int index = 0; index < children.length; index++) {
+                            childDiscs.add(discMap.get(children[index]));
+                        }
+                        discMap.get(discName).children = childDiscs;
+                    }
+                }
+            }
+            bufferedReader.close();
+        } catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");
+        } catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+        Disc root = new Disc("",0);
+        for (String disc : discs) {
+            if (!allChildren.contains(disc)) {
+                System.out.println("Root is named " + disc);
+                root = discMap.get(disc);
+            }
+        }
+        solve(root);
+    }
+
+    public int solve(Disc disc) {
+        if (disc.children.isEmpty()) {
+            return disc.weight;
+        }
+        HashMap<Integer, Integer> weights = new HashMap<Integer, Integer>();
+        ArrayList<Integer> weightList = new ArrayList<Integer>();
+        for (int index = 0; index < disc.children.size(); index++) {
+            int weight = solve(disc.children.get(index));
+            if (weights.containsKey(weight)) {
+                weights.put(weight, weights.get(weight) + 1);
+            } else {
+                weights.put(weight, 1);
+            }
+            weightList.add(weight);
+        }
+        if (weights.keySet().size() > 1) {
+            int wrongWeight = -1;
+            int rightWeight = -1;
+            for (Integer key : weights.keySet()) {
+                if (weights.get(key) == 1) {
+                    wrongWeight = key;
+                } else {
+                    rightWeight = key;
+                }
+            }
+            for (int index = 0; index < weightList.size(); index++) {
+                if (weightList.get(index) == wrongWeight) {
+                    System.out.println("Mismatch weight is on child named " + disc.children.get(index).name + " It is " + wrongWeight + " It should be " + rightWeight);
+                }
+            }
+        }
+        int totalWeight = disc.weight;
+        for (Integer key : weights.keySet()) {
+            totalWeight += key * weights.get(key);
+        }
+        return totalWeight;
+    }
+
+    public void questionSix() {
+        String fileName = System.getProperty("user.dir") + "\\src\\advent\\code\\questionSix.txt";
+        int[] buckets = new int[0];
+        int numBuckets;
+        String line;
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null) {
+                String[] words = line.split("\t");
+                buckets = new int[words.length];
+                for (int index = 0; index < words.length; index++) {
+                    buckets[index] = Integer.parseInt(words[index]);
+                }
+            }
+            bufferedReader.close();
+        } catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file '" + fileName + "'");
+        } catch(IOException ex) {
+            System.out.println("Error reading file '" + fileName + "'");
+        }
+
+        numBuckets = buckets.length;
+        int counter = 0;
+        int maxVolume;
+        int maxBucket;
+        HashSet<String> snapshots = new HashSet<String>();
+        String snapshot = "";
+        for (int index = 0; index < numBuckets; index++) {
+            snapshot += buckets[index] + ",";
+        }
+        while (!snapshots.contains(snapshot)) {
+            snapshots.add(snapshot);
+            snapshot = "";
+            counter++;
+
+            // find the highest value and its owning bucket
+            maxVolume = Integer.MIN_VALUE;
+            maxBucket = -1;
+            for (int index = 0; index < numBuckets; index++) {
+                int current = buckets[index];
+                if (current > maxVolume) {
+                    maxVolume = current;
+                    maxBucket = index;
+                }
+            }
+
+            // distribute the buckets contents to all buckets
+            int currentBucket = maxBucket;
+            buckets[maxBucket] = 0;
+            while (maxVolume > 0) {
+                currentBucket++;
+                if (currentBucket >= numBuckets) {
+                    currentBucket = 0;
+                }
+                buckets[currentBucket]++;
+                maxVolume--;
+            }
+
+            // take a snapshot
+            for (int index = 0; index < numBuckets; index++) {
+                snapshot += buckets[index] + ",";
+            }
+        }
+        System.out.println(counter);
+
+        counter = 0;
+        snapshots = new HashSet<String>();
+        snapshot = "";
+        for (int index = 0; index < numBuckets; index++) {
+            snapshot += buckets[index] + ",";
+        }
+        while (!snapshots.contains(snapshot)) {
+            snapshots.add(snapshot);
+            snapshot = "";
+            counter++;
+
+            // find the highest value and its owning bucket
+            maxVolume = Integer.MIN_VALUE;
+            maxBucket = -1;
+            for (int index = 0; index < numBuckets; index++) {
+                int current = buckets[index];
+                if (current > maxVolume) {
+                    maxVolume = current;
+                    maxBucket = index;
+                }
+            }
+
+            // distribute the buckets contents to all buckets
+            int currentBucket = maxBucket;
+            buckets[maxBucket] = 0;
+            while (maxVolume > 0) {
+                currentBucket++;
+                if (currentBucket >= numBuckets) {
+                    currentBucket = 0;
+                }
+                buckets[currentBucket]++;
+                maxVolume--;
+            }
+
+            // take a snapshot
+            for (int index = 0; index < numBuckets; index++) {
+                snapshot += buckets[index] + ",";
+            }
+        }
+        System.out.println(counter);
+    }
+
+    public void questionFive() {
+        String fileName = System.getProperty("user.dir") + "\\src\\advent\\code\\questionFive.txt";
         ArrayList<Integer> numList = new ArrayList<Integer>();
         String line;
 
         try {
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            int valids = 0;
             while((line = bufferedReader.readLine()) != null) {
-                Integer num =
-                HashSet<String> wordSet = new HashSet<String>();
-                boolean valid = true;
-                for (String word : words) {
-                    if (wordSet.contains(word)) {
-                        valid = false;
-                    }
-                    wordSet.add(word);
-                }
-                if (valid) {
-                    valids++;
-                }
+                Integer num = Integer.parseInt(line);
+                numList.add(num);
             }
             bufferedReader.close();
-            System.out.println(valids);
         } catch(FileNotFoundException ex) {
             System.out.println("Unable to open file '" + fileName + "'");
         } catch(IOException ex) {
             System.out.println("Error reading file '" + fileName + "'");
         }
+        int position = 0;
+        int jump = 0;
+        int next = 0;
+        int[] nums = new int[numList.size()];
+        for (int index = 0; index < nums.length; index++) {
+            nums[index] = numList.get(index).intValue();
+        }
+        while (position < nums.length) {
+            next = nums[position];
+            nums[position]++;
+            position += next;
+            jump++;
+        }
+        System.out.println(jump);
+
+        position = 0;
+        jump = 0;
+        next = 0;
+        nums = new int[numList.size()];
+        for (int index = 0; index < nums.length; index++) {
+            nums[index] = numList.get(index).intValue();
+        }
+        while (position < nums.length) {
+            next = nums[position];
+            if (nums[position] >= 3) {
+                nums[position]--;
+            } else {
+                nums[position]++;
+            }
+            position += next;
+            jump++;
+        }
+        System.out.println(jump);
     }
 
-    public static void questionFour() {
-        String fileName = System.getProperty("user.dir") + "\\src\\com\\company\\questionFour.txt";
+    public void questionFour() {
+        String fileName = System.getProperty("user.dir") + "\\src\\advent\\code\\questionFour.txt";
 
         String line;
 
@@ -101,7 +356,7 @@ public class Main {
         }
     }
 
-    public static HashMap<String, Integer> toLetterCount(String word) {
+    public HashMap<String, Integer> toLetterCount(String word) {
         HashMap<String, Integer> letterCounts = new HashMap<String, Integer>();
         for (int index = 0; index < word.length(); index++) {
             String letter = word.substring(index, index+1);
@@ -114,7 +369,7 @@ public class Main {
         return letterCounts;
     }
 
-    public static boolean containsAnagram(ArrayList<HashMap<String, Integer>> allWordToLetterCounts, HashMap<String, Integer> wordLetterCount) {
+    public boolean containsAnagram(ArrayList<HashMap<String, Integer>> allWordToLetterCounts, HashMap<String, Integer> wordLetterCount) {
         Set<String> wordKeySet = wordLetterCount.keySet();
         for (int index = 0; index < allWordToLetterCounts.size(); index++) {
             HashMap<String, Integer> wordInPassPhraseLetterCount = allWordToLetterCounts.get(index);
@@ -137,7 +392,7 @@ public class Main {
         return false;
     }
 
-    public static void questionThree() {
+    public void questionThree() {
         int input = 312051;
         int layers = 0;
         int last = 0;
@@ -294,8 +549,8 @@ public class Main {
         System.out.println("First value larger than " + input + " is " + lastValue);
     }
 
-    public static void questionTwo() {
-        String fileName = System.getProperty("user.dir") + "\\src\\com\\company\\data.txt";
+    public void questionTwo() {
+        String fileName = System.getProperty("user.dir") + "\\src\\advent\\code\\data.txt";
 
         String line;
 
@@ -352,7 +607,7 @@ public class Main {
         }
     }
 
-    public static void questionOne() {
+    public void questionOne() {
         String code = "7385764686251444473997915123782972536343732657517834671759462795461213782428342931896181695578996274321317419242359534783957372932953774336338118488967172727651862498838195317654289797558683458511126996217953322817229372373455862177844478443391835484591525235651863464891177927244954925827786799436536592561374269299474738321293575385899438446558569241236278779779983587912431395475244796538888373287186921647426866237756737342731976763959499149996315591584716122199183295277439872911371313924594486766479438544417416529743495114819825984524437367225234184772617942525954961136976875325182725754768372684531972614455134523596338355374444273522115362238734383164778129376628621497662965456761631796178353599629887665939521892447361219479646483978798392716119793282717739524897385958273726776318154977675546287789874265339688753977185129334929715486381875286278528247696464162297691698154712775589541945263574897266575996455547625537947927972497979333932115165151462742216327321116291372396585618664475715321298122335789262942284571328414569375464386446824882551918843185195829547373915482687534432942778312542752798313434628498295216692646713137244198123219531693559848915834623825919191532658735422176965451741869666714874158492556445954852299161868651448123825821775363219246244515946392686275545561989355573946924767442253465342753995764791927951158771231944177692469531494559697911176613943396258141822244578457498361352381518166587583342233816989329544415621127397996723997397219676486966684729653763525768655324443991129862129181215339947555257279592921258246646215764736698583211625887436176149251356452358211458343439374688341116529726972434697324734525114192229641464227986582845477741747787673588848439713619326889624326944553386782821633538775371915973899959295232927996742218926514374168947582441892731462993481877277714436887597223871881149693228928442427611664655772333471893735932419937832937953495929514837663883938416644387342825836673733778119481514427512453357628396666791547531814844176342696362416842993761919369994779897357348334197721735231299249116477";
         int sum = 0;
         String numString = code.substring(code.length() - 1, code.length());
